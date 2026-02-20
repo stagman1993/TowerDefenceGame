1886f7515833948cb016f2cdad22a70f4968e475
Joe Wasden <jpwaden93@gmail.com>
2026-02-20 19:56:08 +0000

    Fixed linting errors

--- src/enemy/enemy.py
+++ src/enemy/enemy.py
@@ -1,17 +1,18 @@
 import pygame as pg
 
-class Enemy():
-    def __init__(self, waypoints):
+
+class Enemy:
+    def __init__(self, waypoints: list) -> None:
         self.waypoints = waypoints
         self.pos = pg.Vector2(waypoints[0])
         self.target_waypoint = 1
         self.speed = 5
         self.color = None
 
-    def update(self):
+    def update(self) -> None:
         self.move()
 
-    def move(self):
+    def move(self) -> None:
         if self.target_waypoint < len(self.waypoints):
             self.target = pg.Vector2(self.waypoints[self.target_waypoint])
             self.movement = self.target - self.pos
@@ -29,11 +30,12 @@ class Enemy():
                 self.pos += self.movement.normalize() * dist
             self.target_waypoint += 1
 
-    def kill(self):
+    def kill(self) -> None:
         self.pos = pg.Vector2(self.waypoints[0])
         self.target_waypoint = 0
 
+
 class BlueEnemy(Enemy):
-    def __init__(self, waypoints):
+    def __init__(self, waypoints: list) -> None:
         super().__init__(waypoints)
-        self.color = ("Blue")
+        self.colour = "Blue"
